557895512bd149a5948c231cc405bb1a3e5edcd1
kevin <drzhong2015@gmail.com>
2020-07-30 13:37:55 +0800

    2020年7月30日添加文件mimeType转文件后缀类型

---
+++ src/helper/image/Image.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LXZ
+ * Date: 2020/7/30
+ * Time: 11:12
+ */
+namespace Helper\Image;
+/**
+ * 图片处理类
+ * Class Image
+ * @package Helper\Image
+ */
+class Image
+{
+
+}
