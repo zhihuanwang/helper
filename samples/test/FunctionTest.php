7501ad276630638dc806a11374ef2009cdaae4ab
kevin <drzhong2015@gmail.com>
2020-07-30 13:42:29 +0800

    测试代码部分

---
+++ samples/test/FunctionTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LXZ
+ * Date: 2020/7/30
+ * Time: 13:39
+ */
+
+namespace Test;
+
+
+class FunctionTest
+{
+    public function index()
+    {
+
+    }
+}
---
+++ samples/test/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LXZ
+ * Date: 2020/7/30
+ * Time: 13:38
+ */
+require_once __DIR__ . "/FunctionTest.php";
+(new \Test\FunctionTest())->index();
